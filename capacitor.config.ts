e195cf1a91d05be25a25b5122023041d0c4c82b3
Ezechiel DAGUENET <ezechieldaguenet@hotmail.fr>
2022-12-22 16:50:06 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'premiere-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
